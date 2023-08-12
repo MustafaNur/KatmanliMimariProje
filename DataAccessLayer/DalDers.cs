8314ca66642ed54fdf8e78d313e1ca807c529d57
MustafaNur <mustafa@drupart.co.uk>
2023-08-12 18:17:12 +0300

    Layerlar oluşturuldu

---
+++ DataAccessLayer/DalDers.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer
+{
+    public class DalDers
+    {
+        //crud Metotları
+        //Eklme silme güncelleme listeleme
+        public static int DersEkle(EntityDers parametre)
+        {
+            SqlCommand komut1 = new SqlCommand("insert into tblDersler " +
+                "(DersAd) values (@p1)", Baglanti.bgl);
+            if (komut1.Connection.State != ConnectionState.Open)
+            {
+                komut1.Connection.Open();
+            }
+            komut1.Parameters.AddWithValue("@p1", parametre.DersAd);
+            return komut1.ExecuteNonQuery();
+        }
+        
+    }
+}
---
+++ KatmanliMimariProje/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace KatmanliMimariProje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDersID = new System.Windows.Forms.TextBox();
+            this.txtDersAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(587, 294);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(663, 103);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(110, 30);
+            this.btnKaydet.TabIndex = 1;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(630, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "DersID:";
+            // 
+            // txtDersID
+            // 
+            this.txtDersID.Location = new System.Drawing.Point(700, 28);
+            this.txtDersID.Name = "txtDersID";
+            this.txtDersID.Size = new System.Drawing.Size(100, 26);
+            this.txtDersID.TabIndex = 3;
+            // 
+            // txtDersAd
+            // 
+            this.txtDersAd.Location = new System.Drawing.Point(700, 60);
+            this.txtDersAd.Name = "txtDersAd";
+            this.txtDersAd.Size = new System.Drawing.Size(100, 26);
+            this.txtDersAd.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DersAd:";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(663, 139);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(110, 30);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(663, 175);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(110, 30);
+            this.btnGuncelle.TabIndex = 7;
+            this.btnGuncelle.Text = "Güncelleme";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(663, 211);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(110, 30);
+            this.btnListele.TabIndex = 8;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 326);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.txtDersAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDersID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Ders İşlemleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDersID;
+        private System.Windows.Forms.TextBox txtDersAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnListele;
+    }
+}
+
